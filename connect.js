a2cf79ec87c2204a6e0c58f08ac8de8008118545
phungthuan99 <58097690+phungthuan99@users.noreply.github.com>
2021-10-15 18:19:49 +0700

    new
    new

--- connect.js
+++ connect.js
@@ -1,6 +1,7 @@
 import * as THREE from './three/build/three.module.js';
 import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
+import { ObjectLoader } from './three/build/three.module.js';
 import { DRACOLoader } from './three/examples/jsm/loaders/DRACOLoader.js';
 
 var obj;
@@ -8,6 +9,9 @@ const scene = new THREE.Scene();
 const loader = new GLTFLoader();
 const renderer = new THREE.WebGLRenderer();
 const textrure = new THREE.TextureLoader();
+const blobs = { './can/can.gltf': 'blob1', './can/01.png0': 'blob2', './can/02.png': 'blob3' }
+console.log(blobs);
+let object;
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -16,18 +20,48 @@ const camera = new THREE.PerspectiveCamera(
 );
 const controls = new OrbitControls(camera, renderer.domElement);
 var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 3);
+init();
+animate();
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function init() {
 
-loader.load('./can/can.gltf', function(glft) {
-    obj = glft.scene;
-    scene.add(glft.scene);
-});
-scene.background = new THREE.Color(0x999999);
-scene.add(light);
-camera.position.set(0, 0, 0.2);
-controls.update();
+    function loadModel() {
+
+        object.traverse(function(child) {
+
+            if (child.isMesh) child.material.map = texture;
+
+        });
+
+        object.position.y = -95;
+        scene.add(object);
+
+    }
+
+    const manager = new THREE.LoadingManager();
+    const objectUrls = [];
+    manager.setURLModifier((url) => {
+        url = URL.createObjectURL(blobs[url]);
+        objectUrls.push(url);
+        return url;
+    });
+
+
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    loader.load('./can/can.gltf', function(glft) {
+        scene.add(glft.scene);
+        objectUrls.forEach((url) => URL.revokeObjectURL(url));
+    });
+
+    scene.background = new THREE.Color(0xcccccc);
+    scene.add(light);
+    camera.position.set(0, 0, 0.2);
+    controls.update();
+
+}
 
 function animate() {
     requestAnimationFrame(animate);
