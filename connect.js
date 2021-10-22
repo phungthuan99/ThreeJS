ab2c2353617b741288a6f1277426dcaf0793fab4
phungthuan99 <58097690+phungthuan99@users.noreply.github.com>
2021-10-22 18:38:40 +0700

    22/10
    using image for object

--- connect.js
+++ connect.js
@@ -1,16 +1,23 @@
-import * as THREE from './three/build/three.module.js';
-import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js';
-import { ObjectLoader } from './three/build/three.module.js';
-import { DRACOLoader } from './three/examples/jsm/loaders/DRACOLoader.js';
+import * as THREE from './assets/JS/three.module.js';
+import { GLTFLoader } from './assets/JS/GLTFLoader.js';
+import { OrbitControls } from './assets/JS/OrbitControls.js';
 
-let gltf, cubes, material, texture_material;
+let texture_material, image_img;
 
 function main() {
     const canvas = document.querySelector('#c');
+    const _click = document.querySelector('#click');
+    _click.addEventListener('click', changeImage, false);
+
+    function changeImage(e) {
+        const chk = new PointerEvent(changeImage);
+        console.log('ba', chk);
+        const url = './can/circle.png';
+        return url;
+    }
     const renderer = new THREE.WebGLRenderer({ canvas });
     canvas.addEventListener('mousedown', (e) => {
-        // console.log(e.clientX, e.clientY);
+
         canvas.style.cursor = 'grabbing';
     });
     canvas.addEventListener('mouseup', (e) => {
@@ -25,14 +32,13 @@ function main() {
         1,
         1000
     );
-    camera.position.set(1, 0, 1);
+
     const controls = new OrbitControls(camera, canvas);
     controls.target.set(1, 1, 1);
     controls.update();
     const scene = new THREE.Scene();
     scene.background = new THREE.Color('#F6F6F6');
-    const manager = new THREE.LoadingManager();
-    // console.log('manager', manager);
+
     {
         const skyColor = 0xffffff;
         const groundColor = 0xffffff;
@@ -40,65 +46,68 @@ function main() {
         const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
         scene.add(light);
     } {
-        const ambientlight = new THREE.AmbientLight(0xffffff, 0.8);
+        const ambientlight = new THREE.AmbientLight(0xffffff, 1);
         scene.add(ambientlight);
     } {
         const light = new THREE.DirectionalLight(0xffffff);
-        light.position.set(1, 1, 1);
+        light.position.set(10, 100, 100);
         scene.add(light);
     }
 
     function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
         const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
-        const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
+        const halfFovY = THREE.MathUtils.degToRad(camera.fov * 0.5);
         const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
-        // compute a unit vector that points in the direction the camera is now
-        // in the xz plane from the center of the box
         const direction = (new THREE.Vector3())
             .subVectors(camera.position, boxCenter)
             .multiply(new THREE.Vector3(0, 0, 1))
             .normalize();
+
+
         camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
         camera.near = boxSize / 100;
         camera.far = boxSize * 100;
-        camera.updateProjectionMatrix();
 
         camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+        camera.updateProjectionMatrix();
     }
 
     {
         function onProgress(xhr) {
             if (xhr.lengthComputable) {
-
                 const percentComplete = xhr.loaded / xhr.total * 100;
-                // console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
+
             }
         }
 
         function onError(e) {
-            // console.log('Error', e)
+
         };
         const loading = new GLTFLoader();
+        image_img = './can/circle.png';
         loading.load('./can/can.gltf', (gltf) => {
-            const textureLoader = new THREE.TextureLoader(manager);
-            const texture = textureLoader.load('./can/bcc.jpg');
-            // console.log(texture);
+            const textureLoader = new THREE.TextureLoader();
+            const texture = textureLoader.load(image_img);
+            texture.flipY = false;
             const root = gltf.scene;
             scene.add(root);
+            root.rotation.set(0, 1.5, 0);
             root.traverse((obj) => {
                 if (obj.isMesh) {
-                    let textureImg = obj,
-                        material_c = textureImg.material;
-                    material_c.map = texture;
-                    console.log('debug', material_c);
-                    let material_n = new THREE.MeshPhongMaterial({
-                        color: 0x999999,
-                        map: material_c.map,
-                        transparent: true
-                    });
-                    texture_material = material_n;
-                    texture_material.needsUpdate = true;
-                    textureImg.material = material_n;
+                    if (obj.name == 'label') {
+                        let textureImg = obj,
+                            material_c = textureImg.material;
+                        material_c.map = texture;
+                        let material_n = new THREE.MeshPhongMaterial({
+                            color: 0x999999,
+                            map: material_c.map,
+                            transparent: true
+                        });
+
+                        texture_material = material_n;
+                        texture_material.needsUpdate = true;
+                        textureImg.material = material_n;
+                    }
                 }
             });
             root.updateMatrixWorld();
@@ -111,22 +120,6 @@ function main() {
             controls.update();
         }, onProgress, onError);
     }
-    const loading = new GLTFLoader();
-    const model = new THREE.Object3D();
-    const url = './can/can.gltf';
-    loading.load(url, (gltf) => {
-            gltf.scene = model;
-            // console.log(model)
-            model.name = 'model';
-            scene.add(model);
-            model.position.set(1, 1, 1);
-        },
-        function(e) {
-            // console.log('onProcess', e)
-        },
-        function(e) {
-            // console.log('onError', e)
-        });
 
     function resizeRendererToDisplaySize(renderer) {
         const canvas = renderer.domElement;
